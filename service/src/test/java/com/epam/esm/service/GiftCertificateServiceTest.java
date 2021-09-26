ae1b949a0593e9b5f4a5aaaf3c9c66edc04cac6b
explosion204 <dzmitriy20magic@gmail.com>
2021-09-26 17:05:56 +0300

    added tests for service layer

---
+++ service/src/test/java/com/epam/esm/service/GiftCertificateServiceTest.java
@@ -0,0 +1,261 @@
+package com.epam.esm.service;
+
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.GiftCertificateSearchParamsDto;
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.exception.EntityNotFoundException;
+import com.epam.esm.exception.InvalidEntityException;
+import com.epam.esm.repository.GiftCertificateRepository;
+import com.epam.esm.repository.OrderingType;
+import com.epam.esm.repository.TagRepository;
+import com.epam.esm.validator.GiftCertificateValidator;
+import com.epam.esm.validator.TagValidator;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(PER_CLASS)
+class GiftCertificateServiceTest {
+    private static final ZonedDateTime INITIAL_DATE = LocalDateTime.now().atZone(ZoneOffset.UTC);
+
+    @InjectMocks
+    private GiftCertificateService certificateService;
+
+    @Mock
+    private GiftCertificateRepository certificateRepository;
+
+    @Mock
+    private TagRepository tagRepository;
+
+    @Spy
+    private GiftCertificateValidator certificateValidator;
+
+    @Spy
+    private TagValidator tagValidator;
+
+    @BeforeAll
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFind() {
+        List<GiftCertificate> certificateList = new ArrayList<>() {{
+            add(provideCertificate());
+        }};
+
+        List<GiftCertificateDto> certificateDtoList = new ArrayList<>() {{
+            add(provideCertificateDto());
+        }};
+
+        when(certificateRepository.find(anyString(), anyString(), anyString(), any(OrderingType.class),
+                any(OrderingType.class))).thenReturn(certificateList);
+        when(tagRepository.findByCertificate(anyLong())).thenReturn(provideTags());
+
+        GiftCertificateSearchParamsDto searchParamsDto = provideSearchParamsDto();
+        List<GiftCertificateDto> actualDtoList = certificateService.find(searchParamsDto);
+
+        int expectedInteractions = 1;
+        verify(certificateRepository, times(expectedInteractions)).find(anyString(), anyString(), anyString(),
+                any(OrderingType.class), any(OrderingType.class));
+
+        Assertions.assertEquals(certificateDtoList, actualDtoList);
+    }
+
+    @Test
+    void testFindById() {
+        long certificateId = 1;
+        GiftCertificate certificate = provideCertificate();
+        certificate.setId(certificateId);
+
+        GiftCertificateDto expectedCertificateDto = provideCertificateDto();
+        expectedCertificateDto.setId(certificateId);
+
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.of(certificate));
+        when(tagRepository.findByCertificate(anyLong())).thenReturn(provideTags());
+
+        GiftCertificateDto actualCertificateDto = certificateService.findById(certificateId);
+
+        int expectedInteractions = 1;
+        verify(certificateRepository, times(expectedInteractions)).findById(anyLong());
+        verify(tagRepository, times(expectedInteractions)).findByCertificate(anyLong());
+
+        Assertions.assertEquals(expectedCertificateDto, actualCertificateDto);
+    }
+
+    @Test
+    void testFindByIdWhenCertificateNotFound() {
+        int certificateId = 1;
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> certificateService.findById(certificateId));
+    }
+
+    @Test
+    void testCreate() {
+        GiftCertificateDto certificateDto = provideCertificateDto();
+
+        certificateService.create(certificateDto);
+
+        int expectedInteractions = 1;
+        verify(certificateValidator, times(expectedInteractions)).validate(any(GiftCertificate.class), eq(false));
+        verify(certificateRepository, times(expectedInteractions)).create(any());
+    }
+
+    @Test
+    void testCreateWhenCertificateInvalid() {
+        GiftCertificateDto certificateDto = provideCertificateDto();
+        certificateDto.setName(null);
+
+        Assertions.assertThrows(InvalidEntityException.class, () -> certificateService.create(certificateDto));
+    }
+
+    @Test
+    void testUpdate() {
+        Tag tag = provideTags().get(0);
+
+        long certificateId = 1;
+        GiftCertificate certificate = provideCertificate();
+        certificate.setId(certificateId);
+        GiftCertificateDto updatedCertificateDto = provideCertificateDto();
+        updatedCertificateDto.setId(certificateId);
+
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.of(certificate));
+        when(certificateRepository.update(any(GiftCertificate.class))).thenReturn(true);
+        when(tagRepository.findByCertificate(anyLong())).thenReturn(provideTags());
+        when(tagRepository.findByName(anyString())).thenReturn(Optional.empty());
+
+        certificateService.update(updatedCertificateDto);
+
+        int expectedInteractions = 1;
+        verify(certificateValidator, times(expectedInteractions)).validate(any(GiftCertificate.class), eq(true));
+        verify(certificateRepository, times(expectedInteractions)).update(any(GiftCertificate.class));
+        verify(tagValidator, times(expectedInteractions)).validate(anyString());
+        verify(tagRepository, times(expectedInteractions)).findByName(anyString());
+        verify(certificateRepository, times(expectedInteractions)).attachTag(anyLong(), anyLong());
+    }
+
+    @Test
+    void testUpdateWhenCertificateNotFound() {
+        GiftCertificateDto updatedCertificateDto = provideCertificateDto();
+
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.empty());
+        Assertions.assertThrows(EntityNotFoundException.class, () -> certificateService.update(updatedCertificateDto));
+    }
+
+    @Test
+    void testUpdateWhenCertificateInvalid() {
+        GiftCertificate certificate = provideCertificate();
+        GiftCertificateDto updatedCertificateDto = provideCertificateDto();
+        updatedCertificateDto.setName("");
+
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.of(certificate));
+
+        Assertions.assertThrows(InvalidEntityException.class, () -> certificateService.update(updatedCertificateDto));
+    }
+
+    @Test
+    void testUpdateWhenTagInvalid() {
+        GiftCertificate certificate = provideCertificate();
+        GiftCertificateDto updatedCertificateDto = provideCertificateDto();
+        updatedCertificateDto.setTags(new ArrayList<>() {{ add(""); }});
+
+        when(certificateRepository.findById(anyLong())).thenReturn(Optional.of(certificate));
+        when(certificateRepository.update(any(GiftCertificate.class))).thenReturn(true);
+
+        Assertions.assertThrows(InvalidEntityException.class, () -> certificateService.update(updatedCertificateDto));
+    }
+
+    @Test
+    void testDelete() {
+        when(certificateRepository.delete(anyLong())).thenReturn(true);
+
+        int certificateId = 1;
+        certificateRepository.delete(certificateId);
+
+        int expectedInteractions = 1;
+        verify(certificateRepository, times(expectedInteractions)).delete(anyLong());
+    }
+
+    @Test
+    void testDeleteWhenCertificateNotFound() {
+        when(certificateRepository.delete(anyLong())).thenReturn(false);
+
+        int certificateId = 1;
+        Assertions.assertThrows(EntityNotFoundException.class, () -> certificateService.delete(certificateId));
+
+        int expectedInteractions = 1;
+        verify(certificateRepository, times(expectedInteractions)).delete(anyLong());
+    }
+
+    private GiftCertificate provideCertificate() {
+        GiftCertificate certificate = new GiftCertificate();
+        certificate.setName("certificate");
+        certificate.setDescription("description");
+        certificate.setPrice(BigDecimal.ONE);
+        certificate.setDuration(Duration.ofDays(1));
+        certificate.setCreateDate(INITIAL_DATE);
+        certificate.setLastUpdateDate(INITIAL_DATE);
+
+        return certificate;
+    }
+
+    private GiftCertificateDto provideCertificateDto() {
+        GiftCertificateDto certificateDto = new GiftCertificateDto();
+        certificateDto.setName("certificate");
+        certificateDto.setDescription("description");
+        certificateDto.setPrice(BigDecimal.ONE);
+        certificateDto.setDuration(Duration.ofDays(1));
+        certificateDto.setCreateDate(INITIAL_DATE);
+        certificateDto.setLastUpdateDate(INITIAL_DATE);
+        certificateDto.setTags(provideTagNames());
+
+        return certificateDto;
+    }
+
+    private GiftCertificateSearchParamsDto provideSearchParamsDto() {
+        GiftCertificateSearchParamsDto searchParamsDto = new GiftCertificateSearchParamsDto();
+
+        searchParamsDto.setTagName("tag");
+        searchParamsDto.setCertificateName("certificate");
+        searchParamsDto.setCertificateDescription("description");
+        searchParamsDto.setOrderByCreateDate(OrderingType.ASC);
+        searchParamsDto.setOrderByName(OrderingType.ASC);
+
+        return searchParamsDto;
+    }
+
+    private List<Tag> provideTags() {
+        Tag tag = new Tag();
+
+        tag.setId(1);
+        tag.setName("tag");
+
+        return new ArrayList<>() {{
+            add(tag);
+        }};
+    }
+
+    private List<String> provideTagNames() {
+        return new ArrayList<>() {{
+           add("tag");
+        }};
+    }
+}
---
+++ service/src/test/java/com/epam/esm/service/TagServiceImplTest.java
@@ -0,0 +1,179 @@
+package com.epam.esm.service;
+
+import com.epam.esm.dto.TagDto;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.exception.EntityAlreadyExistsException;
+import com.epam.esm.exception.EntityNotFoundException;
+import com.epam.esm.exception.InvalidEntityException;
+import com.epam.esm.repository.TagRepository;
+import com.epam.esm.validator.TagValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestInstance(PER_CLASS)
+class TagServiceImplTest {
+    @InjectMocks
+    private TagService tagService;
+
+    @Mock
+    private TagRepository tagRepository;
+
+    @Spy
+    private TagValidator tagValidator;
+
+    @BeforeAll
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindAll() {
+        when(tagRepository.findAll()).thenReturn(provideTagsList());
+
+        List<TagDto> expectedDtoList = provideTagDtoList();
+        List<TagDto> actualDtoList = tagService.findAll();
+
+        Assertions.assertEquals(expectedDtoList, actualDtoList);
+    }
+
+    @Test
+    void testFindById() {
+        when(tagRepository.findById(anyLong())).thenReturn(Optional.of(provideTag()));
+
+        int tagId = 1;
+        TagDto expectedDto = provideTagDto();
+        TagDto actualDto = tagService.findById(tagId);
+
+        Assertions.assertEquals(expectedDto, actualDto);
+    }
+
+    @Test
+    void testFindByIdWhenTagNotFound() {
+        when(tagRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        int tagId = 1;
+        Assertions.assertThrows(EntityNotFoundException.class, () -> tagService.findById(tagId));
+    }
+
+    @Test
+    void testCreate() {
+        TagDto tagDto = provideTagDto();
+
+        when(tagRepository.findByName(anyString())).thenReturn(Optional.empty());
+
+        tagService.create(tagDto);
+
+        int expectedInteractions = 1;
+        verify(tagValidator, times(expectedInteractions)).validate(anyString());
+        verify(tagRepository, times(expectedInteractions)).findByName(anyString());
+        verify(tagRepository, times(expectedInteractions)).create(any(Tag.class));
+    }
+
+    @Test
+    void testCreateWhenTagInvalid() {
+        TagDto tagDto = provideTagDto();
+        tagDto.setName("");
+
+        Assertions.assertThrows(InvalidEntityException.class, () -> tagService.create(tagDto));
+    }
+
+    @Test
+    void testCreateWhenTagAlreadyExists() {
+        TagDto tagDto = provideTagDto();
+
+        when(tagRepository.findByName(anyString())).thenReturn(Optional.of(provideTag()));
+        Assertions.assertThrows(EntityAlreadyExistsException.class, () -> tagService.create(tagDto));
+
+        int expectedInteractions = 1;
+        verify(tagValidator, times(expectedInteractions)).validate(anyString());
+    }
+
+    @Test
+    void testDelete() {
+        when(tagRepository.delete(anyLong())).thenReturn(true);
+
+        int tagId = 1;
+        tagService.delete(tagId);
+
+        int expectedInteractions = 1;
+        verify(tagRepository, times(expectedInteractions)).delete(anyLong());
+    }
+
+    @Test
+    void testDeleteWhenTagNotFound() {
+        when(tagRepository.delete(anyLong())).thenReturn(false);
+
+        int tagId = 1;
+        Assertions.assertThrows(EntityNotFoundException.class, () -> tagService.delete(tagId));
+
+        int expectedInteractions = 1;
+        verify(tagRepository, times(expectedInteractions)).delete(anyLong());
+    }
+
+    private Tag provideTag() {
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("tag");
+
+        return tag;
+    }
+
+    private TagDto provideTagDto() {
+        TagDto tagDto = new TagDto();
+        tagDto.setId(1);
+        tagDto.setName("tag");
+
+        return tagDto;
+    }
+
+    private List<Tag> provideTagsList() {
+        Tag firstTag = new Tag();
+        firstTag.setId(1);
+        firstTag.setName("tag1");
+
+        Tag secondTag = new Tag();
+        secondTag.setName("tag2");
+
+        Tag thirdTag = new Tag();
+        thirdTag.setName("tag3");
+
+        return new ArrayList<>() {{
+            add(firstTag);
+            add(secondTag);
+            add(thirdTag);
+        }};
+    }
+
+    private List<TagDto> provideTagDtoList() {
+        TagDto firstTagDto = new TagDto();
+        firstTagDto.setId(1);
+        firstTagDto.setName("tag1");
+
+        TagDto secondTagDto = new TagDto();
+        secondTagDto.setName("tag2");
+
+        TagDto thirdTagDto = new TagDto();
+        thirdTagDto.setName("tag3");
+
+        return new ArrayList<>() {{
+            add(firstTagDto);
+            add(secondTagDto);
+            add(thirdTagDto);
+        }};
+    }
+}
