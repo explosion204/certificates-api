10b25cbdec923e3bd40160f69ed9e11814e7536c
explosion204 <dzmitriy20magic@gmail.com>
2021-10-07 21:35:17 +0300

    find most widely used tag fix

--- repository/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
+++ repository/src/main/java/com/epam/esm/repository/impl/TagRepositoryImpl.java
@@ -22,8 +22,9 @@ public class TagRepositoryImpl implements TagRepository {
             SELECT t.id, t.name
             FROM app_user AS u
             INNER JOIN app_order AS o ON o.id_user = u.id
-            INNER JOIN gift_certificate AS c ON o.id_certificate = c.id
-            INNER JOIN certificate_tag AS ct ON c.id = ct.id_certificate
+            INNER JOIN certificate_order AS co ON co.id_order = o.id
+            INNER JOIN gift_certificate AS c ON c.id = co.id_certificate
+            INNER JOIN certificate_tag AS ct ON ct.id_certificate = c.id
             INNER JOIN tag AS t ON ct.id_tag = t.id
             WHERE u.id = (
                 SELECT u.id
