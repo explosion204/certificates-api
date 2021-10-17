1390299a9ce8af17c8490f2c42edac04822ec8aa
explosion204 <dzmitriy20magic@gmail.com>
2021-10-17 22:44:12 +0300

    fixed postgresql-specific syntax

--- web/src/main/resources/schema.sql
+++ web/src/main/resources/schema.sql
@@ -1,52 +1,50 @@
 CREATE TABLE gift_certificate (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    id BIGSERIAL PRIMARY KEY, -- the same as BIGINT
     name VARCHAR(50) NOT NULL,
     description VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) UNSIGNED NOT NULL,
-    duration INT UNSIGNED NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    duration INT NOT NULL,
     create_date TIMESTAMP NOT NULL,
-    last_update_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
+    last_update_date TIMESTAMP NOT NULL
 );
 
 CREATE TABLE tag (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-    name VARCHAR(50) UNIQUE,
-    PRIMARY KEY (id)
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE app_user (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE app_order (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
-    cost DECIMAL(10, 2) UNSIGNED NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    cost DECIMAL(10, 2) NOT NULL,
     purchase_date TIMESTAMP NOT NULL,
-    id_user BIGINT UNSIGNED NOT NULL,
+    id_user BIGINT NOT NULL,
 
     CONSTRAINT user_fk FOREIGN KEY (id_user) REFERENCES app_user (id)
 );
 
 CREATE TABLE certificate_tag (
-    id_certificate BIGINT UNSIGNED NOT NULL,
-    id_tag BIGINT UNSIGNED NOT NULL,
+    id_certificate BIGINT NOT NULL,
+    id_tag BIGINT NOT NULL,
 
     CONSTRAINT ct_certificate_fk FOREIGN KEY (id_certificate) REFERENCES gift_certificate (id) ON DELETE CASCADE,
     CONSTRAINT ct_tag_fk FOREIGN KEY (id_tag) REFERENCES tag (id) ON DELETE CASCADE
 );
 
 CREATE TABLE certificate_order (
-    id_certificate BIGINT UNSIGNED NOT NULL,
-    id_order BIGINT UNSIGNED NOT NULL,
+    id_certificate BIGINT NOT NULL,
+    id_order BIGINT NOT NULL,
 
     CONSTRAINT co_certificate_fk FOREIGN KEY (id_certificate) REFERENCES gift_certificate (id) ON DELETE CASCADE,
     CONSTRAINT co_order_fk FOREIGN KEY (id_order) REFERENCES app_order (id)
 );
 
 CREATE TABLE audit_table (
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+    id BIGSERIAL PRIMARY KEY,
     operation VARCHAR(20),
     entity_name VARCHAR(50),
     timestamp TIMESTAMP
